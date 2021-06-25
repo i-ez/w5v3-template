987e1fa6e53aad8d32b4b5f5722fb9b112eba0fe
QuYang <jscoder@qq.com>
2021-06-25 18:07:00 +0800

    入口文件修改

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>webpack5+Vue3</h1>
+    <p>{{name}}</p>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  setup() {
+    const name = ref("解析Vue文件");
+
+    return {
+      name,
+    };
+  },
+});
+</script>
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('this is index.js');
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
