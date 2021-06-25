f170f6eb60c5d08f5db6e0f51a77e26c78d66ce2
QuYang <jscoder@qq.com>
2021-06-25 11:57:26 +0800

    生产环境配置css资源处理

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,8 +1,41 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../"
+            }
+          },
+          'css-loader',
+          'postcss-loader',
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../"
+            }
+          },
+          'css-loader',
+          'postcss-loader',
+          'less-loader'
+        ]
+      },
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'W5V3',
@@ -16,6 +49,17 @@ module.exports = {
         minifyCSS: true // 压缩内联css
       },
       cache: true
+    }),
+    // CSS文件压缩
+    new OptimizeCSSAssetsPlugin({
+      cssProcessor: require('cssnano'), // OptimizeCSSAssetsPlugin插件默认也是使用cssnano
+      cssProcessorOptions: {
+        discardComments: { removeAll: true }
+      }
+    }),
+    // 提取单独CSS文件
+    new MiniCssExtractPlugin({
+      filename: 'css/[name]-[contenthash:6].css'
     }),
     new CleanWebpackPlugin()
   ]
